91427ea07ebb8e8642ce71e50dcf860181e77169
Ammar1999y <thesyclashers.911@gmail.com>
2024-10-08 23:53:19 +0300

    Commit message

--- app/api/redirect/route.js
+++ app/api/redirect/route.js
@@ -1,8 +1,8 @@
 export async function GET() {
   return Response.json({ redirectByClient: "/home" }, {
-    status: 200,
+    status: 307,
     headers: {
-      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Origin": "https://localhost", 
       "Content-Type": "application/json; charset=utf-8"
     },
   }); 
@@ -11,7 +11,7 @@ export function OPTIONS() {
   return new Response(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://localhost",
       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
     },
   });
